bc728bf5a194783bc427957870c74a07166b240d
MyungireJ <john.myungire@gmail.com>
2014-12-13 09:29:41 +0200

    My first file

---
+++ Understand.php
@@ -0,0 +1,163 @@
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>
+	POM SOLUTIONS &#124; We care..</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="http://www.pom.solutions/xmlrpc.php" />
+<link rel="alternate" type="application/rss+xml" title="POM SOLUTIONS &raquo; Feed" href="http://www.pom.solutions/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="POM SOLUTIONS &raquo; Comments Feed" href="http://www.pom.solutions/?feed=comments-rss2" />
+<link rel="alternate" type="application/rss+xml" title="POM SOLUTIONS &raquo; Home2 Comments Feed" href="http://www.pom.solutions/?feed=rss2&#038;page_id=2" />
+<link rel='stylesheet' id='esteem_style-css'  href='http://www.pom.solutions/wp-content/themes/esteem/style.css?ver=3.9.2' type='text/css' media='all' />
+<link rel='stylesheet' id='esteem-responsive-css'  href='http://www.pom.solutions/wp-content/themes/esteem/css/responsive.css?ver=3.9.2' type='text/css' media='all' />
+<link rel='stylesheet' id='esteem-fontawesome-css'  href='http://www.pom.solutions/wp-content/themes/esteem/fontawesome/css/font-awesome.css?ver=3.2.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://www.pom.solutions/wp-includes/js/jquery/jquery.js?ver=1.11.0'></script>
+<script type='text/javascript' src='http://www.pom.solutions/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='http://www.pom.solutions/wp-content/themes/esteem/js/esteem-custom.js?ver=3.9.2'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.pom.solutions/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.pom.solutions/wp-includes/wlwmanifest.xml" /> 
+<link rel='next' title='Articles Page' href='http://www.pom.solutions/?page_id=20' />
+<meta name="generator" content="WordPress 3.9.2" />
+<link rel='canonical' href='http://www.pom.solutions/' />
+<link rel='shortlink' href='http://www.pom.solutions/' />
+
+<!--Plugin Global Hide Admin Tool Bar 1.6.1 Build 2014-05-31 Active - Tag 74be16979710d4c4e7c6647856088456-->
+
+<!-- This website is patched against a big problem not solved from WordPress 3.3+ to date -->
+
+
+<!--Plugin Global Hide Admin Tool Bar Bruteforce 1.6.1 Build 2014-04-16 Active - Tag 74be16979710d4c4e7c6647856088456-->
+
+<!-- This website is patched against a big core annoyance since WordPress 3.3+ to date -->
+
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+<style type="text/css" id="custom-background-css">
+body.custom-background { background-color: #cce698; }
+</style>
+</head>
+
+<body class="home page page-id-2 page-template page-template-page-templatesbusiness-php logged-in custom-background no-sidebar">
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+			<div class="inner-wrap">
+				<div class="hgroup-wrap clearfix">
+					<div class="site-branding">
+											</div><!-- .site-branding -->
+					
+				</div><!-- .hgroup-wrap -->
+			</div><!-- .inner-wrap -->
+					<img src="http://www.pom.solutions/wp-content/uploads/2014/11/cropped-logoFinal1.png" class="header-image" width="380" height="165" alt="POM SOLUTIONS">
+	
+				<div class="slider-wrap">
+		<div class="slider-cycle">
+								<section id="featured-slider" class="slides displayblock">
+						<figure class="slider-image-wrap">
+							<img alt="" src="http://www.pom.solutions/wp-content/uploads/2014/11/fourthImage.jpg">
+					    </figure>	
+					    					</section><!-- .featured-slider -->
+									<section id="featured-slider" class="slides displaynone">
+						<figure class="slider-image-wrap">
+							<img alt="" src="http://www.pom.solutions/wp-content/uploads/2014/11/sixthImage.jpg">
+					    </figure>	
+					    					</section><!-- .featured-slider -->
+									<section id="featured-slider" class="slides displaynone">
+						<figure class="slider-image-wrap">
+							<img alt="" src="http://www.pom.solutions/wp-content/uploads/2014/11/seventhImage.jpg">
+					    </figure>	
+					    					</section><!-- .featured-slider -->
+						</div>
+		<nav id="controllers" class="clearfix">
+		</nav><!-- #controllers -->
+	</div><!-- .slider-cycle -->
+		</header><!-- #masthead -->
+		<div id="main" class="site-main inner-wrap">
+
+	<div id="content" class="site-content">
+		<section id="esteem_call_to_action_widget-3" class="widget widget-home widget-call-to-action clearfix">				<div class="call-to-action clearfix">
+					<div class="call-to-action-text">
+						<div class="call-to-action-text-inner-wrap">
+													<h4>Here you can  put any marketing statement eg bness slogan</h4>
+							
+						</div>					
+					</div><!-- .call-to-action-text -->
+									</div><!-- .call-to-action clearfix -->
+		</section><section id="esteem_testimonial_widget-3" class="widget widget-home widget_testimonial clearfix">					<div class="fancy-tab">
+				<h6>Our Main Products</h6>			</div>
+				<div class="testimonial">
+			<div class="tg-one-half">
+				<div class="testimonial-wrap">
+					<div class="testimonial-content clearfix">
+						<div class="author-image">
+							<img title="author" alt="author" src="http://www.annesimmonds.ca/wp-content/uploads/2011/07/Sacred-Spin-Small-Acrylic-12X12-2011-250..jpg">
+						</div><!-- .testimonial-content -->
+						<div class="testimonial-text">
+							<p>We produce</p>
+						</div><!-- .testimonial-text -->
+					</div><!-- .testimonial-content -->
+					<div class="testimonial-byline">
+						John Gabriel						<span class="author-desc"> <a href="" title=""></a></span>
+					</div><!-- .byline -->
+				</div><!-- .testimonial-wrap -->
+			</div><!-- .tg-one-half -->
+
+			<div class="tg-one-half tg-one-half-last">
+				<div class="testimonial-wrap">
+					<div class="testimonial-content clearfix">
+						<div class="author-image">
+							<img title="author" alt="author" src="http://ih0.redbubble.net/image.23385435.2198/tb,1200x1200,small.2u5.jpg">
+						</div><!-- .testimonial-content -->
+						<div class="testimonial-text">
+							<p>This was so great</p>
+						</div><!-- .testimonial-text -->
+					</div><!-- .testimonial-content -->
+					<div class="testimonial-byline">
+						Another one						<span class="author-desc"> <a href = "" title=""></a></span>
+					</div><!-- .byline -->
+				</div><!-- .testimonial-wrap -->
+			</div><!-- .tg-one-half -->
+		</div><!-- .testimonial -->
+
+		</section>	</div><!-- content -->
+
+</div><!--#main -->
+<footer id="colophon" class="clearfix">
+	
+<div class="widget-wrap inner-wrap clearfix">
+	<div class="tg-one-third">
+		<aside id="text-6" class="widget widget_text"><h3 class="widget-title"><span>FIRST LEFT</span></h3>			<div class="textwidget"><p>Donec nec consectetur lorem. Curabitur iaculis sed ligula in efficitur. Duis eu augue sed arcu lacinia maximus. Vestibulum lacinia tortor orci. Sed ultricies nec augue molestie scelerisque. Curabitur luctus cursus libero non fringilla. In feugiat sagittis imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+</div>
+		</aside>	</div><!-- .tg-one-third -->
+
+	<div class="tg-one-third">
+		<aside id="text-4" class="widget widget_text"><h3 class="widget-title"><span>The Middle One</span></h3>			<div class="textwidget">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper nisl et ultrices varius. Quisque sit amet neque a sapien facilisis dapibus id et enim. Nam tristique, odio in imperdiet tempor, nunc libero euismod odio, malesuada tincidunt sem quam non nulla. Proin id odio non tellus vestibulum bibendum. Phasellus eu lorem quis nunc iaculis sodales. Morbi eleifend sem ipsum.</div>
+		</aside><aside id="text-7" class="widget widget_text">			<div class="textwidget"></div>
+		</aside>	</div><!-- .tg-one-third -->
+
+	<div class="tg-one-third tg-one-third-last">
+		<aside id="text-3" class="widget widget_text"><h3 class="widget-title"><span>RIGHT SIDE CORNER</span></h3>			<div class="textwidget">Donec nec consectetur lorem. Curabitur iaculis sed ligula in efficitur. Duis eu augue sed arcu lacinia maximus. Vestibulum lacinia tortor orci. Sed ultricies nec augue molestie scelerisque. Curabitur luctus cursus libero non fringilla. In feugiat sagittis imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</div>
+		</aside>	</div><!-- .last -->
+</div><!-- .widget-wrap -->	<div id="site-generator" class="inner-wrap">
+		<div class="copyright">Copyright &copy; 2014 <a href="http://www.pom.solutions/" title="POM SOLUTIONS" ><span>POM SOLUTIONS</span></a>.     </div>	</div><!-- #site-generator -->
+</footer>
+<a href="#masthead" id="scroll-up"><i class="icon-angle-up"></i></a>
+</div>
+
+<!--Plugin Global Hide Admin Tool Bar 1.6.1 Build 2014-05-31 Active - Tag 74be16979710d4c4e7c6647856088456-->
+
+<!-- This website is patched against a big problem not solved from WordPress 3.3+ to date -->
+
+
+<!--Plugin Global Hide Admin Tool Bar Bruteforce 1.6.1 Build 2014-04-16 Active - Tag 74be16979710d4c4e7c6647856088456-->
+
+<!-- This website is patched against a big core annoyance since WordPress 3.3+ to date -->
+
+<script type='text/javascript' src='http://www.pom.solutions/wp-includes/js/comment-reply.min.js?ver=3.9.2'></script>
+<script type='text/javascript' src='http://www.pom.solutions/wp-content/themes/esteem/js/jquery.cycle.all.min.js?ver=2.9999.5'></script>
+<script type='text/javascript' src='http://www.pom.solutions/wp-content/themes/esteem/js/esteem-slider-setting.js?ver=3.9.2'></script>
+<script type='text/javascript' src='http://www.pom.solutions/wp-content/themes/esteem/js/navigation.js?ver=3.9.2'></script>
+</body>
+</html>
